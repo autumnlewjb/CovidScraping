7be6b475f36f82d15dc2e29ff0700f8a3ab3868d
autumnlewjb <49812781+autumnlewjb@users.noreply.github.com>
2020-05-05 19:21:35 +0800

    improvements

--- covid_scrapping.py
+++ covid_scrapping.py
@@ -1,12 +1,21 @@
 from bs4 import BeautifulSoup
 import urllib.request
 from time import sleep
+from datetime import datetime
+
+current = datetime.now()
+day = current.day
+month = current.month
+year = current.year
+
+date = "{}/{}/{}".format(day, month, year)
+
 
 file = "latest_covid_cases.csv"
-f = open(file,'a+')
-f.write("\n")
+f = open(file, 'a+')
+f.write("\n" + date + ",")
 source = urllib.request.urlopen("http://www.moh.gov.my/index.php/pages/view/2019-ncov-wuhan").read()
-soup = BeautifulSoup(source,'lxml')
+soup = BeautifulSoup(source, 'lxml')
 
 section1 = soup.body
 titles = []
@@ -17,9 +26,9 @@ for elements in section1.find_all("h3"):
 
 
 if len(titles) == 1:
-    title = titles[0].replace(",","->")
+    title = titles[0].replace(",", "->").strip()
 
-f.write(str(title) + "\n")
+# f.write(str(title) + "\n")
 section2 = soup.center
 # print(section2)
 table = section2.find('table')
@@ -29,8 +38,8 @@ for tr in row:
     td = tr.find_all('td')
     row = [i.text for i in td]
     if len(row) == 2:
-        data_input = str(row[0]) + "," + str(row[1])
-        f.write(data_input + "\n")
+        data_input = str(row[1]) + ","
+        f.write(data_input)
 
 if not(f.closed):
     print("Web Scrapping done...")
