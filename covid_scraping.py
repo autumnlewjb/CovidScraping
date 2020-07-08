5fdf01e887f04d4ef36c831809eafbaa9bdb00a0
Autumn Lew <autumnlewjb@gmail.com>
2020-07-08 16:59:35 +0800

    optimize import

--- covid_scraping.py
+++ covid_scraping.py
@@ -1,13 +1,12 @@
 import re
-from datetime import datetime
-from time import sleep
 import time
+from datetime import datetime
 
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def identify_iframe(tag):
